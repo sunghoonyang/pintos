00c49085e751630a716acd10f7395d69d6570b51
sunghoonyang <sunghoonyang@coupang.com>
2017-10-05 13:44:01 +0900

    patching: 0001-macOS-makefile-build-using-i686-elf-cross-compilers.patch

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@ proj_root=$(pwd)
 ls $proj_root/patches | grep macOS | while read file;
 do
     git am < $proj_root/patches/$file
+    git add -A
+    git commit -m 'patching: '$file
+    git push origin $(git branch | grep \* | cut -c 3-)
 done
